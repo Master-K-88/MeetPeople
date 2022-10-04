cb1707bfbeb0a874f520e0794a85d654f6335769
Korede Ibrahim Oko-osi <67703771+Master-K-88@users.noreply.github.com>
2022-10-04 14:31:19 +0100

    Initial Commit

---
+++ MeetPeople/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MeetPeople
+//
+//  Created by Prof K on 10/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+            .foregroundColor(.green)
+            .background(Color.blue)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MeetPeople/MeetPeopleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetPeopleApp.swift
+//  MeetPeople
+//
+//  Created by Prof K on 10/3/22.
+//
+
+import SwiftUI
+
+@main
+struct MeetPeopleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
